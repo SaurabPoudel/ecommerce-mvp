0a853dec7fd6bc3ecc0d0dd71dc004459631dbd3
SaurabPoudel <poudelsaurab20@gmail.com>
2024-04-28 07:45:59 +0545

    order , email and stripe webhooks

--- src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -1,9 +1,9 @@
 import db from "@/db/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import fs from "fs/promises";
 
 export async function GET(
-  req: NextResponse,
+  req: NextRequest,
   {
     params: { downloadVerificationId },
   }: { params: { downloadVerificationId: string } }
@@ -12,19 +12,20 @@ export async function GET(
     where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
     select: { product: { select: { filePath: true, name: true } } },
   });
+
   if (data == null) {
     return NextResponse.redirect(
       new URL("/products/download/expired", req.url)
     );
   }
+
   const { size } = await fs.stat(data.product.filePath);
   const file = await fs.readFile(data.product.filePath);
-
-  const extension = data.product.filePath.split("").pop();
+  const extension = data.product.filePath.split(".").pop();
 
   return new NextResponse(file, {
     headers: {
-      "Content-Disposition": `attachment; filename= "${data.product.name}.${extension}"`,
+      "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
       "Content-Length": size.toString(),
     },
   });
